d051dd7974c7fa4c989d317bc44f88a3daace553
rezamike <michael.sanaiha@gmail.com>
2018-08-26 19:04:42 -0700

    pretty much wireframing it up, baby

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    // build players, health, and power level
+    let players = [
+        {
+            name: "Goku", health: 94, level: 4
+        },
+        {
+            name: "Gohan", health: 64, level: 2
+        },
+        {
+            name: "Trunks", health: 73, level: 3
+        },
+        {
+            name: "Majin-Buu", health: 90, level: 4
+        },
+        {
+            name: "Vegeta", health: 81, level: 4
+        }
+    ]
+
+
+    // score building, attack, restart
+
+
+
+})
